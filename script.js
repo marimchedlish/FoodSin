ec0c48d805693bd219495218105399caaa9fd7ee
mchedlish <marimchedlish@gmail.com>
2021-11-13 17:17:12 +0400

    added toggle

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ document.getElementById('click').addEventListener('click', function() {
 alert('Your E-mail has been sent!');
 }
 )
+
+let navigation = document.getElementById('navigation');
+let burger = document.getElementById('burger');
+
+burger.addEventListener('click', function() {
+    navigation.classList.toggle('active');
+})
